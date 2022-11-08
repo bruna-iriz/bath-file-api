ff5ffdabce41e881a4b3d513422041336684b50e
Bruna Iriz <45856882+bruna-iriz@users.noreply.github.com>
2022-11-07 21:31:58 -0300

    ajustes em teste quebrado e atualizacao do readme

--- README.md
+++ README.md
@@ -42,9 +42,9 @@ mvn clean verify
 ```
 | Descrição | Link | Status |
 | ------     | ------ | ------ |
-| Relatório de cobertura de testes | target/site/jacoco/index.html | (em andamento: cobertura atual 66%)
+| Relatório de cobertura de testes | target/site/jacoco/index.html | (em andamento: cobertura atual 73%)
 
-![image](https://user-images.githubusercontent.com/45856882/200409968-cd4f0371-bde8-48dc-a78f-f9073d1ecc28.png)
+![img.png](img.png)![image](https://user-images.githubusercontent.com/45856882/200409968-cd4f0371-bde8-48dc-a78f-f9073d1ecc28.png)
 
 
 ### Documentação da API
--- src/test/java/br/com/biv/conversationsbatchapi/controller/ConversationControllerTest.java
+++ src/test/java/br/com/biv/conversationsbatchapi/controller/ConversationControllerTest.java
@@ -1,92 +1,84 @@
-//package br.com.biv.conversationsbatchapi.controller;
-//
-//import br.com.biv.conversationsbatchapi.domain.Conversation;
-//import br.com.biv.conversationsbatchapi.service.ConversationService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.mockito.Mockito.when;
-//
-//@WebMvcTest(ConversationController.class)
-//class ConversationControllerTest {
-//
-//    @MockBean
-//    ConversationService conversationService;
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//    @Autowired
-//    WebApplicationContext webApplicationContext;
-//
-//    @Test
-//    void shouldCreateMockMvc() {
-//        assertNotNull(mockMvc);
-//    }
-//
-//    @Test
-//    public void shouldReturnSuccess_WhenListAllConversation() throws Exception {
-//
-//        final Conversation conversation = Conversation
-//                .builder()
-//                .data("2022-01-01")
-//                .id(15352L)
-//                .status("Ignorado")
-//                .mensagem("Preciso que resolvam um problema. Estou bem chateado.")
-//                .build();
-//
-//        when(conversationService.listAll())
-//                .thenReturn(List.of(conversation));
-//
-//        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/conversations"))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
-//
-//        assertEquals("15352", conversation.getId().toString());
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnSuccess_WhenListConversationById() throws Exception {
-//        final Conversation conversation = Conversation
-//                .builder()
-//                .data("2022-01-01")
-//                .id(15352L)
-//                .status("Ignorado")
-//                .mensagem("Preciso que resolvam um problema. Estou bem chateado.")
-//                .build();
-//
-//        when(conversationService.findById(conversation.getId()))
-//                .thenReturn(Optional.of(conversation));
-//
-//        this.mockMvc.perform(MockMvcRequestBuilders.get("/v1/conversations/{id}", 15352L))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
-//
-//        assertEquals("15352", conversation.getId().toString());
-//    }
-//
-//    @Test
-//    public void shouldReturnSuccess_WhenDeleteConversationById() throws Exception {
-//
-//        final Conversation conversation = Conversation
-//                .builder()
-//                .data("2022-01-01")
-//                .id(15352L)
-//                .status("Ignorado")
-//                .mensagem("Preciso que resolvam um problema. Estou bem chateado.")
-//                .build();
-//
-//        this.mockMvc.perform(MockMvcRequestBuilders.delete("/v1/conversations/{id}", 15352L))
-//                .andExpect(MockMvcResultMatchers.status().isNoContent());
-//
-//    }
-//}
+package br.com.biv.conversationsbatchapi.controller;
+
+import br.com.biv.conversationsbatchapi.domain.Conversation;
+import br.com.biv.conversationsbatchapi.service.ConversationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static java.util.Optional.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ConversationController.class)
+class ConversationControllerTest {
+
+    @MockBean
+    ConversationService conversationService;
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldCreateMockMvc() {
+        assertNotNull(mockMvc);
+    }
+
+    @Test
+    public void shouldReturnSuccess_WhenListAllConversation() throws Exception {
+
+        Conversation conversation = new Conversation();
+        builderConversation(conversation);
+
+        when(conversationService.listAll()).thenReturn(List.of(conversation));
+
+        this.mockMvc
+                .perform(get("/v1/conversations"))
+                .andExpect(status().isOk());
+
+        verify(conversationService, atLeastOnce()).listAll();
+        assertEquals("15352", conversation.getId().toString());
+
+    }
+
+    @Test
+    public void shouldReturnSuccess_WhenListConversationById() throws Exception {
+        Conversation conversation = new Conversation();
+        builderConversation(conversation);
+
+        when(conversationService.findById(conversation.getId())).thenReturn(of(conversation));
+
+        this.mockMvc
+                .perform(get("/v1/conversations/{id}", 15352L))
+                .andExpect(status().isOk());
+
+        assertEquals("15352", conversation.getId().toString());
+    }
+
+    @Test
+    public void shouldReturnSuccess_WhenDeleteConversationById() throws Exception {
+
+        Conversation conversation = new Conversation();
+        builderConversation(conversation);
+
+        this.mockMvc
+                .perform(delete("/v1/conversations/{id}", 15352L))
+                .andExpect(status().isNoContent());
+
+    }
+
+    private void builderConversation(Conversation conversation) {
+        conversation.setId(15352L);
+        conversation.setData("2022-01-01");
+        conversation.setStatus("Ignorado");
+        conversation.setMensagem("Preciso que resolvam um problema. Estou bem chateado");
+    }
+}
